ceb083bbd4183d161ddb87f1dcb1677ca95261cd
Nikita B. Zuev <nikitazu@gmail.com>
2024-02-17 15:13:44 +0300

    rdpm026: Рефач

--- src/NZ.RdpMaid.App/EventModel/Updates/DownloadUpdateEventModel.cs
+++ src/NZ.RdpMaid.App/EventModel/Updates/DownloadUpdateEventModel.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
@@ -14,13 +13,15 @@ namespace NZ.RdpMaid.App.EventModel.Updates
     internal class DownloadUpdateEventConsumer(
         UpdateViewModel model,
         GithubUpdateClient updateClient,
-        FileStorage fileStorage
+        FileStorage fileStorage,
+        ShellProvider shell
     )
         : INotificationHandler<DownloadUpdateEventModel>
     {
         private readonly UpdateViewModel _model = model ?? throw new ArgumentNullException(nameof(model));
         private readonly GithubUpdateClient _updateClient = updateClient ?? throw new ArgumentNullException(nameof(updateClient));
         private readonly FileStorage _fileStorage = fileStorage ?? throw new ArgumentNullException(nameof(fileStorage));
+        private readonly ShellProvider _shell = shell ?? throw new ArgumentNullException(nameof(shell));
 
         public async Task Handle(DownloadUpdateEventModel notification, CancellationToken ct)
         {
@@ -43,23 +44,20 @@ namespace NZ.RdpMaid.App.EventModel.Updates
             if (response.Status == GithubUpdateClient.DownloadStatus.Failed)
             {
                 _model.AddLogIfNotEmpty(response.Error);
-                _model.AddLog($"Не удалось скачать обновление, попробуйте ещё раз");
+                _model.AddLog("Не удалось скачать обновление, попробуйте ещё раз");
                 _model.CurrentStatus = UpdateViewModel.Status.WaitingForDownloadOrder;
 
                 return;
             }
 
             _model.AddLog($"Скачал {response.Data.Length} байт");
-
             _fileStorage.EnsureDataDirectoryExists();
-            await _fileStorage.CreateBinaryFileAsync("update.zip", response.Data, ct);
 
+            await _fileStorage.CreateBinaryFileAsync("update.zip", response.Data, ct);
             var path = _fileStorage.ResolveFilePath("update.zip");
 
             _model.AddLog($"Записал обновление на диск: {path}");
-
-            Process.Start("explorer.exe", path);
-
+            _shell.OpenFile(path);
             _model.CurrentStatus = UpdateViewModel.Status.WaitingForInstallOrder;
         }
     }
